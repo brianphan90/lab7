dfd4cbf6af4c88b215b37973264332703835c5c0
Charles Zoeller <35240554+czoeller1@users.noreply.github.com>
2019-12-12 00:12:14 -0800

    Added Requirement 3

--- choiceHandler.java
+++ choiceHandler.java
@@ -4,6 +4,7 @@ import java.sql.PreparedStatement;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.sql.ResultSet;
+import java.sql.Date;
 public class choiceHandler {
 
 
@@ -14,6 +15,7 @@ public class choiceHandler {
             case 2:
                 break;
             case 3:
+                updateRes();
                 break;
             case 4:
                 deleteRes();
@@ -27,6 +29,118 @@ public class choiceHandler {
         }
 
     }
+
+   public void updateRes() {
+      try (Connection conn = DriverManager.getConnection(System.getenv("LAB7_JDBC_URL"), System.getenv("LAB7_JDBC_USER"), System.getenv("LAB7_JDBC_PW"))) {
+         Scanner input = new Scanner(System.in);
+         System.out.println("Please enter in valid reservation code to change: ");
+         String code = input.nextLine().trim();
+         Date cIn, cOut;
+         ResultSet res;
+         try(PreparedStatement s = conn.prepareStatement("Select Checkin, Checkout from lab7_reservations where Code = ?")){
+            s.setObject(1, code);
+            res = s.executeQuery();
+            if(res.first()){
+               cIn = res.getDate("Checkin");
+               cOut = res.getDate("Checkout");
+            }
+            else{
+               System.out.println("No reservations found with code: " + code);
+               return;
+            }
+         }
+         System.out.println("Please enter the following values(- to keep existing value):\nFirst Name:");
+         String firstName = input.nextLine().trim();
+         System.out.println("Last Name:");
+         String lastName = input.nextLine().trim();
+         System.out.println("Checkin Date:");
+         String checkIn = input.nextLine().trim();
+         System.out.println("Checkout Date:");
+         String checkOut = input.nextLine().trim();
+         System.out.println("Number of Children:");
+         String children = input.nextLine().trim();
+         System.out.println("Number of Adults:");
+         String adults = input.nextLine().trim();
+         String stmt = "UPDATE lab7_reservations SET ";
+         int i = 1;
+         int pos[] = {0,0,0,0,0,0,0};
+         if(!firstName.equals("-")){
+            stmt = stmt.concat("FirstName = ? ,");
+            pos[0] = i++;
+         }
+         if(!lastName.equals("-")){
+            stmt = stmt.concat("LastName = ? ,");
+            pos[1] = i++;
+         }
+         if(!children.equals("-")){
+            stmt = stmt.concat("Kids = ? ,");
+            pos[2] = i++;
+         }
+         if(!adults.equals("-")){
+            stmt = stmt.concat("Adults = ? ,");
+            pos[3] = i++;
+         }
+         if(!checkIn.equals("-")){
+            cIn = Date.valueOf(checkIn);
+         }
+         if(!checkOut.equals("-")){
+            cOut = Date.valueOf(checkOut);
+         }
+         try(PreparedStatement d = conn.prepareStatement("select * from lab7_reservations where (Checkout > ? and checkin < ?) and room = (select room from lab7_reservations where code = ?) and code != ?;")){
+            d.setObject(1, cIn);
+            d.setObject(2, cOut);
+            d.setObject(3, code);
+            d.setObject(4, code);
+            ResultSet count = d.executeQuery();
+            if(count.next()){
+               pos[4] = 0;
+               pos[5] = 0;
+               System.out.println("New dates conflict with existing reservations");
+            }
+            else{
+               stmt = stmt.concat("Checkin = ? ,");
+               pos[4] = i++;
+               stmt = stmt.concat("Checkout = ? ,");
+               pos[5] = i++;
+            }
+         }
+         pos[6] = i;
+         stmt = stmt.substring(0, stmt.length()-1);
+         stmt = stmt.concat("where code = ?;");
+         try(PreparedStatement ps = conn.prepareStatement(stmt)) {
+            if(pos[0] != 0)
+               ps.setObject(pos[0], firstName);
+            if(pos[1] != 0)
+               ps.setObject(pos[1], lastName);
+            if(pos[2] != 0)
+               ps.setObject(pos[2], Integer.parseInt(children));
+            if(pos[3] != 0)
+               ps.setObject(pos[3], Integer.parseInt(adults));
+            if(pos[4] != 0)
+               ps.setDate(pos[4], cIn);
+            if(pos[5] != 0)
+               ps.setDate(pos[5], cOut);
+            ps.setObject(pos[6], Integer.parseInt(code));
+
+            if(pos[6] != 1){
+               int rowCount = ps.executeUpdate();
+               if(rowCount > 0){
+                  System.out.println("Updated reservation with code: " + code);
+               }
+               else {
+                  System.out.println("No reservations found with code: " + code);
+               }
+            }else{
+               System.out.println("No new information entered");
+            }
+            conn.close();
+         }
+      }catch (Exception e) {
+         System.err.println(e.getMessage());
+         System.err.println("Unable to connect to database");
+         System.exit(1);
+     }
+   }
     public void deleteRes() {
         try (Connection conn = DriverManager.getConnection(System.getenv("LAB7_JDBC_URL"),
                 System.getenv("LAB7_JDBC_USER"), System.getenv("LAB7_JDBC_PW"))) {
